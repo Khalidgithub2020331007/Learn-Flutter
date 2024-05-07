6355512e0086d9dafa488bcefcd78433ba83812e
Khalidgithub2020331007 <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-05-07 13:06:49 +0600

    Create Map List.dart

---
+++ Map List.dart
@@ -0,0 +1,23 @@
+void main() {
+
+  List<Map<String,int>> marks=[
+    {
+      'Math':25,
+      'CS':30,
+      'English':21,
+    },
+    {
+      'Math':25,
+      'CS':35,
+      'English':21,
+    },
+    {
+      'Math':25,
+      'CS':30,
+      'English':31,
+    },
+  ];
+  marks.map((e){
+    print(e);
+  }).toList;
+}
