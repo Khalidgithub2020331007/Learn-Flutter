fad7b083a7b2b78d320816739a4845d6c65f00fc
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-06-17 22:44:24 +0600

    Update future.dart

--- future.dart
+++ future.dart
@@ -41,3 +41,40 @@ Future<String> giveAResultAfter2Sec()
 
 
 // 3...........
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+void main() async {
+  var url = Uri.https('jsonplaceholder.typicode.com', 'users/1');
+  final res = await http.get(url);
+  print(res);
+  print(res.body);
+  print(res.body[0]);
+  print(jsonDecode(res.body));
+  print(jsonDecode(res.body)['name']);
+  var ur=Uri.https('onplaceholder.typicode.com', 'ers/1');
+  try{
+  final r = await http.get(ur);
+  print(r.body);
+  }
+  catch(e)
+  {
+    print('something Error');
+  }
+  
+  
+  http.get(ur).then((val)
+                    {
+                      print(jsonDecode(val.body)['name']);
+                    }).catchError((err)
+  {
+    print(err);
+  });
+  
+  
+}
+
+// Future<String> giveAResultAfter2Sec() {
+//   return Future.delayed(Duration(seconds: 2), () async {
+//     return 'Hey!!!!';
+//   });
+// }
